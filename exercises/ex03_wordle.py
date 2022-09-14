e63b31c34da7a9d772461254bd48c4f0e0738d13
Hayes <hayesh@email.unc.edu>
2022-09-13 23:58:56 -0400

    done

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -18,20 +18,22 @@ def contains_char (search_second: str, single_character: str) -> bool:
     return False
 
 def emojified (six_letter_word: str, secret_word: str) -> str: 
+    """This function checks the user's six_letter_guess against the secret_word to give them the appopriate box.  """
     assert len(six_letter_word) == len(secret_word)
     blank_box: str = ""
     counter: int = 0
     while len(secret_word) > counter:
         if (six_letter_word[counter]) == secret_word[counter]:
             blank_box += GREEN_BOX
-        elif contains_char is True:  
-            blank_box += YELLOW_BOX
+        elif contains_char(secret_word, six_letter_word[counter]) is False:  
+            blank_box += WHITE_BOX 
         else:
-            blank_box += WHITE_BOX
+            blank_box += YELLOW_BOX
         counter += 1
     return blank_box
 
 def input_guess (n: int) -> str:
+    """This function allows the user to insert a guess for the wordle."""
     prompt = input(f"Enter a {n} character word: ")
     while len(prompt) != n:
         prompt = input(f"That wasn't {n} chars! Try again: ")
@@ -39,3 +41,14 @@ def input_guess (n: int) -> str:
 
 def main() -> None: 
     """The entrypoint of the program and main game loop."""
+    turn: int = 0
+    secret_word: str = "codes"
+    while turn <=5: 
+        turn +=1 
+        print(f"=== Turn {turn}/6 ===")
+        guessed_word: str = input_guess(len(secret_word))
+        print(emojified(guessed_word, secret_word))
+        if turn > 5: 
+            print("X/6 - Sorry, try again tomorrow! ")
+        elif guessed_word == secret_word: 
+            print(f"You won in {turn}/6 turns!")
