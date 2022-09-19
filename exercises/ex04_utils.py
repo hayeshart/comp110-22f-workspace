563e1600e3ed9d28ae2ce94f055c2d6d1ea0412c
Hayes <hayesh@email.unc.edu>
2022-09-19 09:12:34 -0400

    done

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,9 +1,13 @@
 """EX04 - 'list' Utility Functions."""
-__author__ =  730549088
+__author__ = 730549088
 
-def all (numbers: list[int], given_number: int) -> bool:
+
+def all(numbers: list[int], given_number: int) -> bool:
+    """This function inspects the list to see if all the values in the list are equal to a given number."""
     counter: int = 0
     checking_variable: bool = True
+    if len(numbers) == 0: 
+        return False
     while counter < len((numbers)): 
         if numbers[counter] != given_number:
             checking_variable = False
@@ -11,7 +15,9 @@ def all (numbers: list[int], given_number: int) -> bool:
         counter += 1
     return checking_variable
 
+
 def max(input: list[int]) -> int:
+    """This function compares the values in your list and returns the highest one."""
     if len(input) == 0: 
         raise ValueError("max() arg is an empty List")
     counter: int = 0
@@ -23,11 +29,11 @@ def max(input: list[int]) -> int:
     return maximum_value
 
 
-def is_equal (list_1: list[int], list_2: list[int]) -> bool: 
+def is_equal(list_1: list[int], list_2: list[int]) -> bool:
+    """This function determines if two lists are equal to each other at every index."""
     counter: int = 0 
     while counter < len(str(list_1)): 
         if list_1 == list_2:
-            return True 
-        else: 
-            return False
-    counter += 1 
+            return True
+        counter += 1 
+    return False
