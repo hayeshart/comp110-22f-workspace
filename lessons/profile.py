8616cbd2ab3178e7f1ebc69dbf1e2a3d96cf29cf
Hayes <hayesh@email.unc.edu>
2022-11-01 15:58:52 -0400

    done

--- lessons/profile.py
+++ lessons/profile.py
@@ -3,3 +3,20 @@
 class Profile: 
     handle: str 
     followers: int 
+    is_private: bool 
+
+    def __init__(self, handle: str): 
+        """Constructor initializes attributes!"""
+        self.handle = handle
+        self.followers = 0 
+        self.is_private = False
+
+    def tweet(self, msg: str) -> None: 
+        """An example of a method."""
+        print(f"@{self.handle} tweets {msg}")
+    
+
+my_profile: Profile = Profile("krisjordan")
+my_profile.tweet("Hello, world.")
+# another_profile: Profile = Profile("kris.jordan")
+# another_profile.handle = "theverge"
